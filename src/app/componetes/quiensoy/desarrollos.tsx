3f0c8607007cdf78d96fd867f1c5b96365310753
Agustin Escudero <agustinescuderoweb@gmail.com>
2025-09-23 21:31:57 -0300

    nuevo comit

--- src/app/componetes/quiensoy/desarrollos.tsx
+++ src/app/componetes/quiensoy/desarrollos.tsx
@@ -8,6 +8,7 @@ import ferrari from '../../../../public/ferrari.jpg'
 import meraki from '../../../../public/meraki.jpg'
 import huellas from '../../../../public/montaña.jpg'
 import analisis from '../../../../public/analisis.jpeg'
+import dentista from '../../../../public/dentista.jpg'
 
 
 function desarrollos() {
@@ -49,6 +50,10 @@ function desarrollos() {
            <a href='https://zestly-0a3ce8.webflow.io/'><div className={styles.card}><Image className={styles.imag} src={analisis} alt="analisis"></Image><h1>Analisis de Datos con IA</h1><h3>Categoría: Inteligencia Artificial</h3>  <button className="relative group bg-[rgb(50,205,50)] text-white px-6 py-3 text-base font-bold rounded-md shadow-lg overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105">
                 <span className="pointer-events-none absolute -left-full top-1/2 w-full h-full -translate-y-1/2 rotate-[-45deg] bg-white opacity-0 transition-all duration-500 ease-in-out group-hover:left-full group-hover:opacity-100"></span>
                 <a href='https://zestly-0a3ce8.webflow.io/' className="relative z-10">Ver web</a>
+                </button></div></a>
+           <a href='https://dentista-smoky.vercel.app'><div className={styles.card}><Image className={styles.imag} src={dentista} alt="dentista"></Image><h1>Dental Studio</h1><h3>Categoría: Dentistas</h3>  <button className="relative group bg-[rgb(50,205,50)] text-white px-6 py-3 text-base font-bold rounded-md shadow-lg overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105">
+                <span className="pointer-events-none absolute -left-full top-1/2 w-full h-full -translate-y-1/2 rotate-[-45deg] bg-white opacity-0 transition-all duration-500 ease-in-out group-hover:left-full group-hover:opacity-100"></span>
+                <a href='https://dentista-smoky.vercel.app' className="relative z-10">Ver web</a>
                 </button></div></a>
         </div>
     </div>
